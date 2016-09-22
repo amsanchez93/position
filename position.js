8a2e4d1bd317ed85a41108322660878b7cde2c85
amsanchez93 <as29@hawaii.edu>
2016-09-21 16:12:47 -1000

    Position Finished
    This version has no ESLint errors.

--- position.js
+++ position.js
@@ -1,12 +1,19 @@
 const testArr = [1, 3, 5, 6];
-const target = 5;
+const testTarget1 = 5;
+const testTarget2 = 2;
+const testTarget3 = 7;
+const testTarget4 = 0;
 
 function position(arr, target) {
-  for (let i = 0; i <= testArr.length; i++) {
-    if (target < arr[i]) {
-      return (i - 1);
+  for (let i = 0; i <= arr.length; i += 1) {
+    if (target <= arr[i]) {
+      return i;
     }
   }
+  return arr.length;
 }
 
-console.log(position(testArr, target));
+console.log(position(testArr, testTarget1));
+console.log(position(testArr, testTarget2));
+console.log(position(testArr, testTarget3));
+console.log(position(testArr, testTarget4));
